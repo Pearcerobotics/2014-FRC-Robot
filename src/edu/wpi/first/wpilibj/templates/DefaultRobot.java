a15116339f893d565a7ceb5957bad41e98c45823
Stuart <Stuart@Puck.home>
2014-02-02 14:44:41 -0600

    Adding in P51 base Classes

--- src/edu/wpi/first/wpilibj/templates/DefaultRobot.java
+++ src/edu/wpi/first/wpilibj/templates/DefaultRobot.java
@@ -18,58 +18,11 @@ import edu.wpi.first.wpilibj.CANJaguar;
 import edu.wpi.first.wpilibj.Jaguar;
 import edu.wpi.first.wpilibj.SpeedController;
 import edu.wpi.first.wpilibj.can.CANTimeoutException;
-
+import edu.wpi.first.wpilibj.Gyro;
+import edu.wpi.first.wpilibj.Encoder;
+import edu.wpi.first.wpilibj.camera.AxisCamera;
 /**
- * This "BuiltinDefaultCode" provides the "default code" functionality as used in the "Benchtop Test."
- *
- * The BuiltinDefaultCode extends the IterativeRobot base class to provide the "default code"
- * functionality to confirm the operation and usage of the core control system components, as
- * used in the "Benchtop Test" described in Chapter 2 of the 2009 FRC Control System Manual.
- *
- * This program provides features in the Disabled, Autonomous, and Teleop modes as described
- * in the benchtop test directions, including "once-a-second" debugging printouts when disabled,
- * a "KITT light show" on the solenoid lights when in autonomous, and elementary driving
- * capabilities and "button mapping" of joysticks when teleoperated.  This demonstration
- * program also shows the use of the user watchdog timer.
- *
- * This demonstration is not intended to serve as a "starting template" for development of
- * robot code for a team, as there are better templates and examples created specifically
- * for that purpose.  However, teams may find the techniques used in this program to be
- * interesting possibilities for use in their own robot code.
- *
- * The details of the behavior provided by this demonstration are summarized below:
- *
- * Disabled Mode:
- * - Once per second, print (on the console) the number of seconds the robot has been disabled.
- *
- * Autonomous Mode:
- * - Flash the solenoid lights like KITT in Knight Rider
- * - Example code (commented out by default) to drive forward at half-speed for 2 seconds
- *
- * Teleop Mode:
- * - Select between two different drive options depending upon Z-location of Joystick1
- * - When "Z-Up" (on Joystick1) provide "arcade drive" on Joystick1
- * - When "Z-Down" (on Joystick1) provide "tank drive" on Joystick1 and Joystick2
- * - Use Joystick buttons (on Joystick1 or Joystick2) to display the button number in binary on
- *   the solenoid LEDs (Note that this feature can be used to easily "map out" the buttons on a
- *   Joystick.  Note also that if multiple buttons are pressed simultaneously, a "15" is displayed
- *   on the solenoid LEDs to indicate that multiple buttons are pressed.)
- *
- * This code assumes the following connections:
- * - Driver Station:
- *   - USB 1 - The "right" joystick.  Used for either "arcade drive" or "right" stick for tank drive
- *   - USB 2 - The "left" joystick.  Used as the "left" stick for tank drive
- *
- * - Robot:
- *   - Digital Sidecar 1:
- *     - PWM 1/3 - Connected to "left" drive motor(s)
- *     - PWM 2/4 - Connected to "right" drive motor(s)
- *
- * The VM is configured to automatically run this class, and to call the
- * functions corresponding to each mode, as described in the IterativeRobot
- * documentation. If you change the name of this class or the package after
- * creating this project, you must also update the manifest file in the resource
- * directory.
+ * Default 1745 Robot.
  */
 public class DefaultRobot extends IterativeRobot {
 	//Robot Wiring Diagram
@@ -88,14 +41,15 @@ public class DefaultRobot extends IterativeRobot {
         //Victors
         //details the port that each victor is attached to on the relay modual
         final static int COMPRESSORVICTOR_ID = 0;
-        //Sulenoids
+        //Colinoids
         //details the port on the relay module that each sulonid is connected
         final static int LAUNCHERSOLINOID_ID = 0;
-        
         //DIO
         final static int PNUMATICPRESSURESENSOR = 0;
-        //AIO
+        final static int FRONTSONICSENSOR = 1;
+        final static int REARSONICSENSOR = 2;
         
+        //AIO
         //JOYSTICKS
         final static int RIGHTJOYSTICK = 1;
         final static int LEFTJOYSTICK = 2;
@@ -161,7 +115,6 @@ public class DefaultRobot extends IterativeRobot {
                 m_leftBackMotor = new CANJaguar(LEFTREARMOTORCAN_ID);
             } catch (CANTimeoutException ex) {
                System.out.println("CAN TIMEOUT EXCEPTION!\n");
-               ex.printStackTrace();
             }
         }
         else
@@ -173,40 +126,38 @@ public class DefaultRobot extends IterativeRobot {
              m_leftBackMotor = new Jaguar(LEFTREARMOTORPWM_ID);
         }
         // Create a robot using standard right/left robot drive on 
-		m_robotDrive = new RobotDrive(m_leftFrontMotor, m_leftBackMotor, 
-                                                m_rightFrontMotor, m_rightBackMotor);
+	m_robotDrive = new RobotDrive(m_leftFrontMotor, m_leftBackMotor, 
+                                      m_rightFrontMotor, m_rightBackMotor);
 
-		m_dsPacketsReceivedInCurrentSecond = 0;
+	m_dsPacketsReceivedInCurrentSecond = 0;
 
-		// Define joysticks being used at USB port #1 and USB port #2 on the Drivers Station
-		m_rightStick = new Joystick(1);
-		m_leftStick = new Joystick(2);
+        // Define joysticks being used at USB port #1 and USB port #2 on the Drivers Station
+	m_rightStick = new Joystick(1);
+	m_leftStick = new Joystick(2);
                 
                 
 
-		// Iterate over all the buttons on each joystick, setting state to false for each
-		int buttonNum = 1;						// start counting buttons at button 1
-		for (buttonNum = 1; buttonNum <= NUM_JOYSTICK_BUTTONS; buttonNum++) {
-			m_rightStickButtonState[buttonNum] = false;
-			m_leftStickButtonState[buttonNum] = false;
-		}
-
-		// Iterate over all the solenoids on the robot, constructing each in turn
-		int solenoidNum = 1;						// start counting solenoids at solenoid 1
-		for (solenoidNum = 0; solenoidNum < NUM_SOLENOIDS; solenoidNum++) {
-			m_solenoids[solenoidNum] = new Solenoid(solenoidNum + 1);
-		}
-
-		// Set drive mode to uninitialized
-		m_driveMode = UNINITIALIZED_DRIVE;
-
-		// Initialize counters to record the number of loops completed in autonomous and teleop modes
-		m_autoPeriodicLoops = 0;
-		m_disabledPeriodicLoops = 0;
-		m_telePeriodicLoops = 0;
-
-		System.out.println("BuiltinDefaultCode Constructor Completed\n");
+	// Iterate over all the buttons on each joystick, setting state to false for each
+	int buttonNum = 1;						// start counting buttons at button 1
+	for (buttonNum = 1; buttonNum <= NUM_JOYSTICK_BUTTONS; buttonNum++) {
+		m_rightStickButtonState[buttonNum] = false;
+		m_leftStickButtonState[buttonNum] = false;
 	}
+
+	// Iterate over all the solenoids on the robot, constructing each in turn
+	int solenoidNum = 1;						// start counting solenoids at solenoid 1
+	for (solenoidNum = 0; solenoidNum < NUM_SOLENOIDS; solenoidNum++) {
+		m_solenoids[solenoidNum] = new Solenoid(solenoidNum + 1);
+	}
+
+	// Set drive mode to uninitialized
+	m_driveMode = UNINITIALIZED_DRIVE;
+	// Initialize counters to record the number of loops completed in autonomous and teleop modes
+	m_autoPeriodicLoops = 0;
+	m_disabledPeriodicLoops = 0;
+	m_telePeriodicLoops = 0;
+	System.out.println("BuiltinDefaultCode Constructor Completed\n");
+}
 
 
 	/********************************** Init Routines *************************************/
